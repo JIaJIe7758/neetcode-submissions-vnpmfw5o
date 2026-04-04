58c417d16248e44e9e0b4d3c92bc5ebcbd60aef7
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-05 00:59:15 +0100

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int ClimbStairs(int n) {  
+        
+        //0-1-1-2-3-5-8
+
+        if(n <= 2) return n;
+
+        int res = 0;
+        int n1 = 1;
+        int n2 = 2;
+
+        for(int i = 3; i <= n; i++) {
+
+            int temp = n1 + n2;
+            n1 = n2;
+            n2 = temp;
+            res = temp;
+        }
+        return res;
+    }
+}

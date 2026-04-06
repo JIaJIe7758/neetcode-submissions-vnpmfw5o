f024ffe8001752ae06ba0d1c7ea95216d003c973
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-06 01:13:49 +0100

    Add: sqrtx - submission-9

---
+++ Data Structures & Algorithms/sqrtx/submission-9.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int MySqrt(int x) { 
+        
+        if(x == 1) return 1;
+        
+        int l = 1;
+        int r = x; // 1-2-3-4-5
+
+        while(l <= r) {
+            int mid = l + (r-l) / 2;
+
+            long xMid = (long)mid * mid;
+
+            if(xMid > x) {
+                r = mid-1;
+            } else if(xMid < x) {
+                l = mid +1;
+            } else {
+                return mid;
+            }
+        }
+        return r;
+    }
+}

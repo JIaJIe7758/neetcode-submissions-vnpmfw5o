c3d7b8873f9ccb1973a41496836baeb4ce6e484e
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-11 02:06:52 +0100

    Add: delete-node-in-a-bst - submission-20

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-20.cs
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode DeleteNode(TreeNode root, int key) {
+
+        if(root == null) {
+            return root;
+        }
+
+        if(root.val > key) {
+            root.left = DeleteNode(root.left, key);
+        } else if(root.val < key) {
+            root.right = DeleteNode(root.right,key);
+        } else {
+            if(root.left == null) {
+                return root.right;
+            } else if(root.right == null) {
+                return root.left;
+            } else {
+                TreeNode min = FindMin(root.right);
+                root.val = min.val;
+                root.right = DeleteNode(root.right,min.val);
+            }
+        }
+        return root;
+
+
+    }
+    public TreeNode FindMin(TreeNode root) {
+        
+        while(root != null && root.left != null) {
+            root = root.left;
+        }
+
+        return root;
+    }
+}

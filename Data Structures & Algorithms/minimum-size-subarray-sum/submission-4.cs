e3de9f1e7613363be5fb3640d3aeac760778cdd2
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-21 01:00:28 +0100

    Add: minimum-size-subarray-sum - submission-4

---
+++ Data Structures & Algorithms/minimum-size-subarray-sum/submission-4.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MinSubArrayLen(int target, int[] nums) {
+        
+         int l = 0;
+       int total = 0; 
+       int res = int.MaxValue;
+
+       for(int r = 0; r < nums.Length; r++) {
+            total += nums[r];
+
+            while(total >= target) {
+                res = Math.Min(res, r-l+1);
+                total -= nums[l];
+                l++;
+            }
+       }
+       return res == int.MaxValue? 0: res;
+    }
+}

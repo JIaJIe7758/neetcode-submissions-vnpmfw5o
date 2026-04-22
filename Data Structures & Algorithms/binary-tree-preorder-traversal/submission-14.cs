79fbfcfa328de66c13b9846ad4d9f1ec63764667
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-22 01:26:50 +0100

    Add: binary-tree-preorder-traversal - submission-14

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-14.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<int> PreorderTraversal(TreeNode root) {
+        var list = new List<int>();
+        PreOrder(root, list);
+        return list;
+
+    }
+
+    public void PreOrder(TreeNode root, List<int> list) {
+        if(root == null) {
+            return;
+        }
+
+        list.Add(root.val);
+        PreOrder(root.left, list);
+        PreOrder(root.right, list);
+    }
+}

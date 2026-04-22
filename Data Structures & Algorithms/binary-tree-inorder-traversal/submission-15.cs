251106ed4f72ec5e05f2f8acdfd53ee5d958a6f0
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-22 01:11:10 +0100

    Add: binary-tree-inorder-traversal - submission-15

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-15.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<int> InorderTraversal(TreeNode root) {
+        
+        var list = new List<int>();
+        InOrder(root,list);
+        return list;
+
+    }
+
+    public void InOrder(TreeNode root, List<int> list) {
+
+        if(root == null) {
+            return;
+        }
+
+        InOrder(root.left, list);
+        list.Add(root.val);
+        InOrder(root.right, list);
+    }
+}

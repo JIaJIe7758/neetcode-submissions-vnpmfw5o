aee82d7721f20367c37a67a4bb36dd0eb167599e
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-04-23 01:17:14 +0100

    Add: binary-tree-postorder-traversal - submission-9

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-9.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public List<int> PostorderTraversal(TreeNode root) {
+        
+        var stack = new Stack<TreeNode>();
+        var list = new List<int>();
+
+        var curr = root;
+
+        while(curr != null || stack.Count > 0) {
+            while(curr != null) {
+                list.Add(curr.val);
+                stack.Push(curr);
+                curr = curr.right;
+            }
+
+            curr = stack.Pop();
+            curr = curr.left;
+        }
+
+        list.Reverse();
+        return list;
+    }
+}

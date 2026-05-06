79f8e28345d2d10878aa3458a1c8084fa4586327
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-05-06 01:34:06 +0100

    Add: delete-node-in-a-bst - submission-25

---
+++ Data Structures & Algorithms/delete-node-in-a-bst/submission-25.cs
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode DeleteNode(TreeNode root, int key) {
+        
+        TreeNode parent = null;
+        TreeNode curr = root;
+
+        while(curr != null && curr.val != key) {
+            parent = curr;
+            if(curr.val > key) {
+                curr = curr.left;
+            } else if(curr.val < key) {
+                curr = curr.right;
+            }
+        }
+
+        if(curr == null) {
+            return root;
+        }
+
+        //case 2children
+        if(curr.left != null && curr.right != null) {
+            TreeNode successor = curr.right;
+            TreeNode parentSuc = curr;
+
+            while(successor.left != null) {
+                parentSuc = successor;
+                successor = successor.left;
+            }
+
+            curr.val = successor.val;
+            curr = successor;
+            parent = parentSuc;
+        }
+
+        //case 1children
+
+        TreeNode child = curr.left != null? curr.left: curr.right;
+
+        if(parent == null) {
+            return child;
+        } else if(parent.left == curr) {
+            parent.left = child;
+        } else if(parent.right == curr) {
+            parent.right = child;
+        }
+
+        return root;
+    }
+}

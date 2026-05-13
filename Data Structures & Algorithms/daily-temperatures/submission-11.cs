05014996fd0c369d1f401f51c4ec15e69b4ff809
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-05-13 01:25:57 +0100

    Add: daily-temperatures - submission-11

---
+++ Data Structures & Algorithms/daily-temperatures/submission-11.cs
@@ -0,0 +1,18 @@
+public class Solution {
+    public int[] DailyTemperatures(int[] temperatures) {
+        
+        var stack = new Stack<int>();
+        var res = new int[temperatures.Length];
+
+        for(int i = 0; i < temperatures.Length; i++) {
+
+            while(stack.Count > 0 && temperatures[i] > temperatures[stack.Peek()]) {
+                int n = stack.Pop();
+                res[n] = i - n;
+            }
+            stack.Push(i);
+        }
+
+        return res;
+    }
+}

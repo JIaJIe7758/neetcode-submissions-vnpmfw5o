866336fb1bc6db058f6000cf210740eb5105ad5e
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-05-15 01:24:21 +0100

    Add: count-good-nodes-in-binary-tree - submission-8

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-8.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int GoodNodes(TreeNode root) {
+        
+        var queue = new Queue<(TreeNode, int)>(); //usamos Tuple, stack/queue apenas aceitam um argumento
+        int res = 0;
+
+        if(root != null) queue.Enqueue((root, int.MinValue));
+
+        while(queue.Count > 0) {
+            var (node, maxVal) = queue.Dequeue();
+
+            if(node.val >= maxVal) {
+                res++;
+            }
+
+            if(node.left != null) queue.Enqueue((node.left, Math.Max(maxVal, node.val)));
+            if(node.right != null) queue.Enqueue((node.right, Math.Max(maxVal, node.val)));
+        }
+
+        return res;
+    }
+}

77769455c45788abab89287b0ce04c4311dbea5f
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-05-21 00:27:04 +0100

    Add: minimum-stack - submission-20

---
+++ Data Structures & Algorithms/minimum-stack/submission-20.cs
@@ -0,0 +1,32 @@
+public class MinStack {
+    List<int> stack;
+    List<int> min;
+    public MinStack() {
+        stack = new List<int>();
+        min = new List<int>();
+    }
+    
+    public void Push(int val) {
+        if(min.Count < 1 || val <= min[min.Count-1]) {
+            min.Add(val);
+        }
+
+        stack.Add(val);
+    }
+    
+    public void Pop() {
+        if(min.Count > 0 && min[min.Count-1] == stack[stack.Count-1]) {
+            min.RemoveAt(min.Count-1);
+        }
+
+        if(stack.Count > 0) stack.RemoveAt(stack.Count-1);
+    }
+    
+    public int Top() {
+        return stack[stack.Count-1];
+    }
+    
+    public int GetMin() {
+        return min[min.Count-1];
+    }
+}

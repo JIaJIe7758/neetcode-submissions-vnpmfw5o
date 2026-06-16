fe9c43bc18517767e04c627fbecf6fc552f53da0
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-06-16 02:32:16 +0100

    Add: binary-tree-right-side-view - submission-10

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-10.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public List<int> RightSideView(TreeNode root) {
+        
+        var list = new List<int>();
+        var queue = new Queue<TreeNode>();
+
+        if(root != null) queue.Enqueue(root);
+
+        while(queue.Count > 0) {
+            int n = queue.Count;
+            TreeNode temp = null;
+            for(int i = 0; i < n; i++) {
+                TreeNode node = queue.Dequeue();
+                temp = node;
+                if(node.left != null) queue.Enqueue(node.left);
+                if(node.right != null) queue.Enqueue(node.right);
+            }
+            list.Add(temp.val);
+        }
+        return list;
+    }
+}

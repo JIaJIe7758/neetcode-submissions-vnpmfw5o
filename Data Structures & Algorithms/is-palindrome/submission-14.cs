c536a0c24dab35a3c22f22cfc70b7728fc26af49
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-06-23 01:40:29 +0100

    Add: is-palindrome - submission-14

---
+++ Data Structures & Algorithms/is-palindrome/submission-14.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        
+        var builder = new StringBuilder();
+
+        foreach(char c in s) {
+            if(Char.IsNumber(c) || Char.IsLetter(c)) {
+                builder.Append(Char.ToLower(c));
+            }
+        }
+
+        int l = 0;
+        int r = builder.Length-1;
+
+        while(l < r) {
+            if(builder[l] != builder[r]) {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}

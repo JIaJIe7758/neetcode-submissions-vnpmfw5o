ddef40ea2951609a7db9337a65f6559554a8399d
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-06-23 02:03:18 +0100

    Add: sqrtx - submission-14

---
+++ Data Structures & Algorithms/sqrtx/submission-14.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public int MySqrt(int x) {
+        
+        int l = 1;
+        int r = x;
+
+        while(l <= r) {
+            int mid = l + (r-l) / 2;
+
+            long test = (long)mid * mid;
+
+            if(test == x) {
+                return mid;
+            } else if(test < x){
+                l = mid+1;
+            } else {
+                r = mid-1;
+            }
+        }
+
+        return r;
+    }
+}

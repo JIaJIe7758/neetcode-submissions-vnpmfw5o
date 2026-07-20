49f743c464d3b78437ef05ce40b000528f3a6025
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-07-21 00:50:36 +0100

    Add: copy-linked-list-with-random-pointer - submission-12

---
+++ Data Structures & Algorithms/copy-linked-list-with-random-pointer/submission-12.cs
@@ -0,0 +1,40 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+    
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+*/
+
+public class Solution {
+    public Node copyRandomList(Node head) {
+        
+        var dict = new Dictionary<Node, Node>();
+        Node curr = head;
+
+        while(curr != null) {
+            dict[curr] = new Node(curr.val);
+            curr = curr.next;
+        }
+
+        curr = head;
+
+        while(curr != null) {
+
+            Node node = curr;
+            dict[curr].next = node.next != null? dict[curr.next]: null;
+            dict[curr].random = node.random != null? dict[curr.random]: null;
+            curr = curr.next;
+        }
+
+        return head == null? head: dict[head];
+
+    }
+}

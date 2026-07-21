fcd177af27f85b5481eeacac603f77fc6940c814
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-07-21 01:20:32 +0100

    Add: design-hashset - submission-14

---
+++ Data Structures & Algorithms/design-hashset/submission-14.cs
@@ -0,0 +1,84 @@
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        next = null;
+    }
+}
+public class MyHashSet {
+
+    public ListNode head;
+
+    public MyHashSet() {
+        head = null;    
+    }
+    
+    public void Add(int key) {
+        
+        if(!Contains(key)) {
+
+            if(head == null) {
+                head = new ListNode(key);
+                return;
+            }
+            var curr = head;
+
+            while(curr != null && curr.next != null) {
+
+                curr = curr.next;
+            }
+
+            curr.next = new ListNode(key);
+        }
+    }
+    
+    public void Remove(int key) {
+        
+        if(Contains(key)) {
+
+            if(head.val == key) {
+                head = head.next;
+                return;
+            }
+
+            var curr = head;
+
+            while(curr != null && curr.next != null) {
+                if(curr.next.val == key) {
+                    break;
+                }
+
+                curr = curr.next;
+            }
+
+            curr.next = curr.next.next;
+        }
+    }
+    
+    public bool Contains(int key) {
+        
+        if(head == null) return false;
+
+        var curr = head;
+
+        while(curr != null) {
+            if(curr.val == key) {
+                return true;
+            }
+
+            curr = curr.next;
+        }
+
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.Add(key);
+ * obj.Remove(key);
+ * bool param_3 = obj.Contains(key);
+ */

61d8cf25ac4935455e0299647cc70b7d0b680b52
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-07-24 01:28:40 +0100

    Add: buy-and-sell-crypto - submission-16

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-16.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        
+        int l = 0;
+        int maxProfit = 0;
+
+        for(int r = 1; r < prices.Length; r++) {
+            int profit = prices[r] - prices[l];
+
+            if(profit <= 0) {
+                l = r;
+            }
+
+            maxProfit = Math.Max(maxProfit, profit);
+
+        }
+        return maxProfit;
+    }
+}

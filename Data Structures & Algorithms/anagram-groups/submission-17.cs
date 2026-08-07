d1f8e7007d583d828374a36660df01b978015eff
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-07 01:50:45 +0100

    Add: anagram-groups - submission-17

---
+++ Data Structures & Algorithms/anagram-groups/submission-17.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        
+        var dict = new Dictionary<string, List<string>>();
+
+        foreach(string s in strs) {
+            var count = new int[26];
+
+            for(int i = 0; i < s.Length; i++) {
+                count[s[i] - 'a']++;
+            }
+
+            string temp = String.Join(",", count);
+
+            if(!dict.ContainsKey(temp)) {
+                dict[temp] = new List<string>();
+            }
+
+            dict[temp].Add(s);
+        }
+
+        var res = new List<List<string>>(dict.Values);
+        return res;
+    }
+}

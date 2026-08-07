30426e35e11d6c98ab7e6807de2e3e30ade67e10
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-07 02:36:07 +0100

    Add: combinations - submission-4

---
+++ Data Structures & Algorithms/combinations/submission-4.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public List<List<int>> Combine(int n, int k) {
+        
+        var curComb = new List<int>();
+        var combs = new List<List<int>>();
+        Helper(1, curComb, combs, n, k);
+        return combs;
+
+    }
+
+    public void Helper(int i, List<int> curComb, List<List<int>> combs, int n, int k) {
+
+        if(curComb.Count == k) {
+            combs.Add(new List<int>(curComb));
+            return;
+        }
+
+        if(i > n) {
+            return;
+        }
+
+        for(int j = i; j < n + 1; j++) {
+            curComb.Add(j);
+            Helper(j+1, curComb, combs, n, k);
+            curComb.RemoveAt(curComb.Count-1);
+        }
+
+        
+    }
+}

d693dd7a26c235f1ebf075f522aa2551d4175d17
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-13 02:15:11 +0100

    Add: eating-bananas - submission-15

---
+++ Data Structures & Algorithms/eating-bananas/submission-15.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int h) {
+        
+        int l = 1;
+        int r = piles.Max();
+        int res = r;
+
+        while(l <= r) {
+
+            int mid = l + (r-l) / 2;
+            int totalHours = 0;
+
+            for(int i = 0; i < piles.Length; i++) {
+                totalHours += (int)Math.Ceiling(piles[i] / (double)mid);
+            }
+
+            if(totalHours <= h) {
+                r = mid-1;
+                res = mid;
+            } else if(totalHours > h) {
+                l = mid+1;
+            }
+        }
+
+        return res;
+    }
+}

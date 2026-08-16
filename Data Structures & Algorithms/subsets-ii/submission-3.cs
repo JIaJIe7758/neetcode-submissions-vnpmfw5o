b37104769550bdbdb323b274f348dca453c48f29
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-16 01:32:02 +0100

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<int>> SubsetsWithDup(int[] nums) {
+        Array.Sort(nums);
+        var curSet = new List<int>();
+        var subSet = new List<List<int>>();
+        Helper(0, nums, curSet, subSet);
+        return subSet;
+    }
+    public void Helper(int i, int[] nums, List<int> curSet, List<List<int>> subSet) { //1-1-2
+        if(i >= nums.Length) {
+            subSet.Add(new List<int>(curSet));
+            return;
+        }
+
+        curSet.Add(nums[i]);
+        Helper(i+1, nums, curSet, subSet);
+        curSet.RemoveAt(curSet.Count-1);
+
+        while(i + 1 < nums.Length && nums[i] == nums[i+1]) {
+            i++;
+        }
+
+        Helper(i+1, nums, curSet, subSet);
+    }
+}

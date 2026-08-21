5b2becfe5f5c3a179c0ab332934141010a8fdc96
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-21 02:03:15 +0100

    Add: design-browser-history - submission-4

---
+++ Data Structures & Algorithms/design-browser-history/submission-4.cs
@@ -0,0 +1,52 @@
+public class Node {
+    public Node next;
+    public Node prev;
+    public string val;
+
+    public Node(string val) {
+        this.val = val;
+        next = null;
+        prev = null;
+    }
+}
+public class BrowserHistory {
+    private Node curr;
+
+    public BrowserHistory(string homepage) {
+        curr = new Node(homepage);
+    }
+    
+    public void Visit(string url) {
+        curr.next = null;
+        var node = new Node(url);
+        node.prev = curr;
+        curr.next = node;
+        curr = curr.next;
+    }
+    
+    public string Back(int steps) {
+        
+        while(curr.prev != null && steps > 0) {
+            curr = curr.prev;
+            steps--;
+        }
+        return curr.val;
+    }
+    
+    public string Forward(int steps) {
+        
+        while(curr.next != null && steps > 0) {
+            curr = curr.next;
+            steps--;
+        }
+        return curr.val;
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.Visit(url);
+ * string param_2 = obj.Back(steps);
+ * string param_3 = obj.Forward(steps);
+ */

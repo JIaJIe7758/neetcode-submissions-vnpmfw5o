55b237bcca307e40accc54907877502b24f84da4
Pedro Wang <140560870+JIaJIe7758@users.noreply.github.com>
2026-08-31 02:26:32 +0100

    Add: count-good-nodes-in-binary-tree - submission-22

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-22.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int GoodNodes(TreeNode root) {
+        
+        return Dfs(root, root.val);
+    }
+    public int Dfs(TreeNode root, int max) {
+        if(root == null) {
+            return 0;
+        }
+
+        int res = root.val >= max? 1:0;
+        max = Math.Max(root.val, max);
+        res += Dfs(root.left, max);
+        res += Dfs(root.right, max);
+        return res;
+    }
+}
